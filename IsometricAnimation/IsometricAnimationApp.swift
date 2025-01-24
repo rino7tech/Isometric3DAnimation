9dd5b7c0bab2f5db001d1843c41814a9d8bc54f3
rino7tech <rino7.tech@gmail.com>
2025-01-24 15:51:07 +0900

    first-commit

---
+++ IsometricAnimation/IsometricAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricAnimationApp.swift
+//  IsometricAnimation
+//
+//  Created by 伊藤璃乃 on 2025/01/24.
+//
+
+import SwiftUI
+
+@main
+struct IsometricAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
